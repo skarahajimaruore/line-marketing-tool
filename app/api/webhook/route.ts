80270d53f535584ac1816bfe55e5eaf8a9bc4b6e
Shinnosuke Mitsuda <tmh44132436@gmail.com>
2026-03-24 17:53:24 +0900

    ANON_KEYに変更

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,9 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const { MessagingApiClient } = messagingApi;
 
-// 1. Supabaseの初期化
+// 💡 修正ポイント：Vercelにある名前に合わせる
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!; // ここをANON_KEYに変更！
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
